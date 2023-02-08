a9c572bf3639f2bf7aff31012e176bd6e7c4c873
etosolo322 <etosolo322@yandex.ru>
2023-02-08 19:43:14 +0300

    add pretty leftMenu

--- src/components/leftMenu.jsx
+++ src/components/leftMenu.jsx
@@ -1,25 +1,33 @@
 import { Link } from 'react-router-dom'
 import { Algorithm, Camera, Dashboard } from '../assets/svg/SVGcomponent'
 import logo from '../assets/svg/icon.svg'
+import { useState } from 'react'
 
 export const LeftMenu = () =>{
+    const [useless, setUseless] = useState(false)
+
+    const send = () =>{
+        setUseless(!useless)
+    }
     return(
     <aside className="leftMenu">
-        <img src={logo} alt='logo'/>
+        <Link to='/' onClick={send}>
+            <img src={logo} alt='logo' />
+        </Link>
         <ul>
-            <li className='active'>
-                <Link to='dashboard'>
+            <li className={window.location.pathname.includes('dashboard') ? 'active' : 'noActive'}>
+                <Link to='dashboard' onClick={send}>
                     <Dashboard />
                     <span>Dashboard</span>
                 </Link>
             </li>
-            <li className='noActive'> 
-                <Link to='camera'>
+            <li className={window.location.pathname.includes('camera') ? 'active' : 'noActive'}>
+                <Link to='camera' onClick={send}>
                     <Camera/>
                     <span>Camera</span>
                 </Link>
             </li>
-            <li className='noActive'>
+            <li className={window.location.pathname.includes('algorithm') ? 'active' : 'noActive'}>
                 <Algorithm/>
                 <span>Algorithm</span>
             </li>
