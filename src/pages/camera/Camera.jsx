33c73c999fe0126916709dea4ede30fa9cda61a8
etosolo322 <etosolo322@yandex.ru>
2023-02-09 10:01:19 +0300

    added header to proxy

--- src/pages/camera/Camera.jsx
+++ src/pages/camera/Camera.jsx
@@ -2,10 +2,14 @@ import './Camera.scss';
 import { API_CAMERA } from '../../api/api';
 import axios from "axios";
 import { useEffect } from 'react';
+import { proxy } from '../../api/proxy';
 
 function Camera() {
 
   useEffect(()=>{
+    proxy(API_CAMERA, "GET",{
+      
+    })
     axios.get(API_CAMERA).then(data => {
       // setData(data.data)
       console.log(data)
