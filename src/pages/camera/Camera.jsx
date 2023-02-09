d908ca8cca9d9d57dab6a984ee9dfcb91d25e080
etosolo322 <etosolo322@yandex.ru>
2023-02-09 11:20:15 +0300

    change to local

--- src/pages/camera/Camera.jsx
+++ src/pages/camera/Camera.jsx
@@ -6,7 +6,7 @@ import { proxy } from '../../api/proxy';
 function Camera() {
 
   useEffect(()=>{
-    proxy('internet',API_CAMERA, "GET",{
+    proxy('local',API_CAMERA, "GET",{
         'Authorization': "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjc1OTU0NTAzLCJqdGkiOiI4N2NlYjIwNTg2YWM0YzUyYTM0MGYzMTk3ODIyZTZiMiIsInVzZXJfaWQiOjF9.cQsQQW6FS2nrN2oR7mQ2AgyH_WJ7lfrmP7KitisQz2Q"
     })
     .then(data => {
