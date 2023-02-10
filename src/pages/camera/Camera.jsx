d1ee27e8f54e5977455881934153a97a649a11b5
etosolo322 <etosolo322@yandex.ru>
2023-02-10 16:22:35 +0300

    add redirect

--- src/pages/camera/Camera.jsx
+++ src/pages/camera/Camera.jsx
@@ -3,14 +3,18 @@ import { API_CAMERA } from '../../api/api';
 import { useEffect } from 'react';
 import { proxy } from '../../api/proxy';
 import { useCookies } from 'react-cookie';
+import axios from 'axios';
 function Camera() {
 
   const [cookies, setCookie] = useCookies(['token']);
 
   useEffect(()=>{
-    proxy(API_CAMERA, "GET",{
-      'Authorization': cookies.token
-    })
+
+    axios.post("https://5scontrol.pl/proxy_to_ngrok",{
+            url: API_CAMERA,
+            method: "GET",
+            headers: {'Authorization': cookies.token},
+        })
     .then(data => {
       // setData(data.data)
       console.log(data)
