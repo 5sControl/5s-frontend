f01a5d34486b6fd0b35c4eb1848122debf1e3163
etosolo322 <etosolo322@yandex.ru>
2023-02-10 16:23:44 +0300

    add camera

--- src/pages/camera/Camera.jsx
+++ src/pages/camera/Camera.jsx
@@ -3,13 +3,16 @@ import { API_CAMERA } from '../../api/api';
 import { useEffect } from 'react';
 import { proxy } from '../../api/proxy';
 import { useCookies } from 'react-cookie';
+import axios from 'axios';
 function Camera() {
 
   const [cookies, setCookie] = useCookies(['token']);
 
   useEffect(()=>{
-    proxy(API_CAMERA, "GET",{
-      'Authorization': cookies.token
+    axios.get("192.168.1.101:8008/find_cameras",{
+      headers: {
+        'Authorization': cookies.token
+      },
     })
     .then(data => {
       // setData(data.data)
