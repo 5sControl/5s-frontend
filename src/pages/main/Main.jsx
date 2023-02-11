6a261dec922b907358f7c8e80c3400300ee560b0
etosolo322 <etosolo322@yandex.ru>
2023-02-11 23:21:06 +0300

    add start setup screen

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,5 +1,15 @@
+import './main.scss'
+import logo from '../../assets/svg/icon.svg'
 export const Main = () =>{
     return (
-        <>sdf</>
+        <div className='main'>
+        <img src={logo} alt='logo' className='main__logo'/>
+        <span className='main__span'>
+            Congratulations! <br/>You have successfully installed the 5S Control’s Docker and now ready to use it. Complete the setup to start.
+        </span>
+        <button className='main__start'>
+            Start Setup
+        </button>
+        </div>
     )
 }
