3a0502cdcae61267a13de2d8eb1e0e70d576c8f5
etosolo322 <etosolo322@yandex.ru>
2023-02-11 23:00:40 +0300

    add company tab

--- src/assets/svg/SVGcomponent.js
+++ src/assets/svg/SVGcomponent.js
@@ -1,5 +1,6 @@
 import { ReactComponent as Algorithm } from './algorithm.svg'
 import { ReactComponent as Camera } from './camera.svg'
 import { ReactComponent as Dashboard }  from './dashboard.svg'
+import { ReactComponent as Company }  from './company.svg'
 
-export {Algorithm, Camera, Dashboard}
+export {Algorithm, Camera, Dashboard, Company}
