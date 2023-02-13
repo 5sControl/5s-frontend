f16be8e28c21c49ae9dcc84d43dc5d738782383e
etosolo322 <etosolo322@yandex.ru>
2023-02-13 11:55:39 +0300

    true false

--- src/api/api.js
+++ src/api/api.js
@@ -1,16 +1,16 @@
-// const url = "http://0.0.0.0:8080"
+// const url = "http://0.0.0.0:8080/"
 
-// const url = "https://a91c-134-17-26-206.eu.ngrok.io"
-const url = "http://django:8000"
+// const url = "https://5cd7-134-17-26-206.eu.ngrok.io/"
+const url = "http://192.168.1.101/"
 
-export const API_URL = `${url}/api/safety_control/safety/action/`
-export const API_IMAGES = `${url}/`
-export const API_STATIC_MEDIA = `${url}/static/`
-export const API_CAMERA = `http://face_recognition_queue:8008/find_cameras/`
-export const API_USERLIST = `${url}/api/staff_control/employees/admin/`
-export const API_REGISTRATION = `${url}/register/`
-export const API_AUTH = `${url}/auth/jwt/create/`
+export const API_URL = `${url}api/safety/action/`
+export const API_IMAGES = url
+export const API_STATIC_MEDIA = `${url}static/`
+export const API_CAMERA = `${url}find_cameras/`
+export const API_USERLIST = `${url}api/employees/admin/`
+export const API_REGISTRATION = `${url}register/`
+export const API_AUTH = `${url}auth/jwt/create/`
 
 export const API_DASHBOARD_PAGE = (page) =>{
-    return `${API_URL}?page=${page}`
+    return `${url}api/safety/action/?page=${page}`
 }
