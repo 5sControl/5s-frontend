b9770cb4adeac5e03446e30f3db497d27224706c
etosolo322 <etosolo322@yandex.ru>
2023-02-13 16:19:03 +0300

    add only hostname

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@
 
 // const url = "https://a91c-134-17-26-206.eu.ngrok.io"
 // const url = "http://192.168.1.101"
-const url = window.location.host
+const url = window.location.hostname
 export const API_URL = `${url}/api/safety_control/safety/action/`
 export const API_IMAGES = `${url}/`
 export const API_STATIC_MEDIA = `${url}/static/`
