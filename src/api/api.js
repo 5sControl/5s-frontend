87b9766ba68ed824621571c795793b50964169d7
etosolo322 <etosolo322@yandex.ru>
2023-02-13 12:55:34 +0300

    delete host

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,12 @@
 // const url = "http://0.0.0.0:8080"
 
 // const url = "https://a91c-134-17-26-206.eu.ngrok.io"
-const url = "django:8000"
+const url = "http://django"
 
 export const API_URL = `${url}/api/safety_control/safety/action/`
 export const API_IMAGES = `${url}/`
 export const API_STATIC_MEDIA = `${url}/static/`
-export const API_CAMERA = `face_recognition_queue:8008/find_cameras/`
+export const API_CAMERA = `http://face_recognition_queue:8008/find_cameras/`
 export const API_USERLIST = `${url}/api/staff_control/employees/admin/`
 export const API_REGISTRATION = `${url}/register/`
 export const API_AUTH = `${url}/auth/jwt/create/`
