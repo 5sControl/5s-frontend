8fdc47ad84bb32b4f9c46c302bc7223fe33d9207
etosolo322 <etosolo322@yandex.ru>
2023-02-20 09:30:40 +0300

    pretty report

--- src/components/reports/ReportListItem.jsx
+++ src/components/reports/ReportListItem.jsx
@@ -5,11 +5,32 @@ export const ReportListItem = ({item, setCurrentReport, currentReport}) =>{
     return (
         <>
            <div className='dashboard__reports_item' onClick={()=>setCurrentReport(item)}>
-                {  (window.location.pathname.includes('safety') || window.location.pathname.includes('dashboard'))&& <div className={currentReport.id === item.id ? 'dashboard__reports_item_title active': 'dashboard__reports_item_title'}>{item.date_created}</div>}
-                { (window.location.pathname.includes('idle') || window.location.pathname.includes('machine'))  && <div className={currentReport.id === item.id ? 'dashboard__reports_item_title active': 'dashboard__reports_item_title'}>{moment(new Date(item.start_tracking)).format('YYYY-MM-DD')} |  {moment(new Date(item.start_tracking)).format('HH:mm:ss') } - {moment(new Date(item.stop_tracking)).format('HH:mm:ss') }</div>}
+                { (window.location.pathname.includes('safety') || window.location.pathname.includes('dashboard'))   && 
+                    <div 
+                        className={currentReport.id === item.id ? 'dashboard__reports_item_title active': 'dashboard__reports_item_title'}
+                    >
+                        {item.date_created}
+                    </div>
+                }
+                { (window.location.pathname.includes('idle') || window.location.pathname.includes('machine'))  && 
+                    <div 
+                        className={currentReport.id === item.id ? 'dashboard__reports_item_title active': 'dashboard__reports_item_title'}
+                    >
+                            {moment(new Date(item.start_tracking)).format('YYYY-MM-DD')} |  {moment(new Date(item.start_tracking)).format('HH:mm:ss') } - {moment(new Date(item.stop_tracking)).format('HH:mm:ss') }
+                    </div>
+                }
                 <div>{`# ${item.id}`}</div>
                 <div><Camera/> {item.camera}</div>
-                <div><Algorithm/> Safety control:{item.action}</div>
+                {
+                  window.location.pathname.includes('safety') &&  <div><Algorithm/> Safety control:{item.action}</div>
+                }
+                {
+                  window.location.pathname.includes('idle') &&  <div><Algorithm/> Idle control</div>
+                }
+                {
+                  window.location.pathname.includes('machine') &&  <div><Algorithm/> Machine control</div>
+                }
+                
             </div>
         </>
     )
