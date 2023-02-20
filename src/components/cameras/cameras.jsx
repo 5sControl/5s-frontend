9471e9e0d0081b14b4414708ffcd11706315137d
etosolo322 <etosolo322@yandex.ru>
2023-02-20 17:23:02 +0300

    new ngrok

--- src/components/cameras/cameras.jsx
+++ src/components/cameras/cameras.jsx
@@ -1,33 +1,31 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
-import {AiOutlineRight } from "react-icons/ai";
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import { Close } from "../../assets/svg/SVGcomponent";
+
 import { useCookies } from "react-cookie";
+import { CamerasModal } from './modal/camerasModal';
 
 import './cameras.scss'
-import { patchCamera, postCamera } from "../../api/requestHomeAndOffice";
 export const Cameras = () => {
 
     const [cookies, setCookie] = useCookies(['token']);
     const [camerasList, setCamerasList] = useState(false)
     const [isShowModal, setIsShowModal] = useState(false)
-    const [stage, setStage] = useState('selectCamera')
-    const [IPCamera, setIPCamera] = useState('')
-    const [password, setPassword] = useState('')
-    const [username, setUsername] = useState('')
     const [createdCameras, setCreatedCameras] = useState([])
-    const [connectMessage, setConnectMessage] = useState('')
+    
     const [error, setError] = useState(false)
-    const [cameraName, setCameraName] = useState('')
+    
 
     useEffect(() => {
         
         if (window.location.hostname.includes('localhost')) {
 
         axios.get(`http://192.168.1.101:8008/find_cameras/`)
-            .then(response => {setCamerasList(response.data.results)})
+            .then(response => {
+                setCamerasList(response.data.results)
+                console.log(response.data.results)
+            })
             .catch((error)=>setError(error.message))
 
         axios.get(`http://192.168.1.101/api/staff_control/locations/camera/`,{
@@ -66,28 +64,6 @@ export const Cameras = () => {
         setIsShowModal(true)
     }
 
-    const changeCameraName = () => {
-        setIsShowModal(false)
-        setStage('selectCamera') 
-        patchCamera(window.location.hostname, IPCamera, cameraName)
-            .then((res)=>console.log(res))
-    }
-
-    const connect = () => {
-    postCamera(window.location.hostname , IPCamera, username, password, cookies.token )
-       .then((e)=>
-            {   
-                console.log(e.data)
-                localStorage.setItem(e.data.ip, e.data.snapshot)
-                if (!e.data.message.includes('failed')){
-                    setStage('cameraCreated')
-                }else{
-                    console.log(e.data.message)
-                    setConnectMessage(e.data.message)
-                }
-            })
-    }
-
     return (
         <section className="cameras">
             <div className='cameras__title'>
@@ -108,78 +84,16 @@ export const Cameras = () => {
             }
             {error && <div style={{color:'red', fontSize:'26px'}}>{error}</div>}
             {
-                isShowModal &&
-                    <div className='cameras__modal'>
-                        <div className='cameras__modal__container'>
-                        { 
-                        stage === 'selectCamera' && camerasList.length > 0 && 
-                            <>
-                            <div className='cameras__modal__title'>
-                                <h2>Select a camera from your local network</h2>
-                                <Close onClick={() => setIsShowModal(false)} className="cameras__modal__title_close"/>
-                            </div>
-                            <div className='cameras__modal__list'>
-                                {
-                                    camerasList.map((el, ind) => 
-                                        <div key ={ind} 
-                                            className='cameras__modal__list_item' 
-                                            onClick={() =>  {setStage('logAndPass'); 
-                                            setIPCamera(el)}}
-                                        >
-                                            <span>{el}</span>
-                                            <AiOutlineRight/>
-                                        </div>
-                                    )
-                                }
-                            </div>   
-                            </>
-                        }  
-                         { 
-                        stage === 'logAndPass' && 
-                            <>
-                                <div className='cameras__modal__title'>
-                                    <h2>{IPCamera}</h2>
-                                    <Close onClick={() => setIsShowModal(false)} className="cameras__modal__title_close"/>
-                                </div>
-                                <div className='cameras__modal__login'>
-                                    <label>Username</label>
-                                    <input 
-                                        type='text' 
-                                        placeholder='Username'
-                                        value={username}
-                                        onChange={(e) => setUsername(e.target.value)}
-                                        onFocus={() => setConnectMessage('')}
-                                        />
-                                    <label>Password</label>
-                                    <input 
-                                        type='password' 
-                                        placeholder='Password'
-                                        value={password}
-                                        onChange={(e) => setPassword(e.target.value)}
-                                        onFocus={() => setConnectMessage('')}
-                                    />
-                                    <div style={{color:'red'}}>{connectMessage}</div>
-                                    <div className='cameras__modal__login__footer'>
-                                        <button className='cameras__modal__login__cancel' onClick={() => setStage('selectCamera')}>Cancel</button>
-                                        <button className="cameras__modal__login__create" onClick={connect}>Connect</button>
-                                    </div>
-                                </div>   
-                            </>
-                        } 
-                         { 
-                        stage === 'cameraCreated' && 
-                            <>
-                                <div className='cameras__modal__showCamera'>
-                                    <img src={`data:image/png;base64, ${localStorage.getItem(IPCamera)}`} alt='camera'/>
-                                    <input type="text" value={cameraName} onChange={(e) => setCameraName(e.target.value)}/>
-                                    <div className='cameras__modal__login__footer'>
-                                        <button className="cameras__modal__login__create" onClick={changeCameraName}>Save</button>
-                                    </div>
-                                </div>   
-                            </>
-                        } 
-                        </div>
-                    </div>
+                isShowModal && 
+                <>
+                <CamerasModal 
+                    setIsShowModal = {(e) => setIsShowModal(e)}
+                    cookies = {cookies}
+                    camerasList = {camerasList}
+                />
+                {console.log(camerasList)}
+                </>
+                  
             }
         </section>
     )
---
+++ src/components/cameras/modal/camerasModal.jsx
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import { Close } from "../../../assets/svg/SVGcomponent";
+import {AiOutlineRight } from "react-icons/ai";
+import { patchCamera, postCamera } from "../../../api/requestHomeAndOffice";
+
+export const CamerasModal = ({setIsShowModal, cookies, camerasList}) => {
+
+    console.log(camerasList);
+    const [stage, setStage] = useState('selectCamera')
+    const [IPCamera, setIPCamera] = useState('')
+    const [password, setPassword] = useState('')
+    const [username, setUsername] = useState('')
+    const [connectMessage, setConnectMessage] = useState('')
+    const [cameraName, setCameraName] = useState('')
+
+    const changeCameraName = () => {
+        setIsShowModal(false)
+        setStage('selectCamera') 
+        patchCamera(window.location.hostname, IPCamera, cameraName)
+            .then((res)=>console.log(res))
+    }
+    
+    const connect = () => {
+        postCamera(window.location.hostname , IPCamera, username, password, cookies.token )
+           .then((e)=>
+                {   
+                    console.log(e.data)
+                    localStorage.setItem(e.data.ip, e.data.snapshot)
+                    if (!e.data.message.includes('failed')){
+                        setStage('cameraCreated')
+                    }else{
+                        console.log(e.data.message)
+                        setConnectMessage(e.data.message)
+                    }
+                })
+        }
+
+    return (
+        <div className='cameras__modal'>
+        <div className='cameras__modal__container'>
+        { 
+        stage === 'selectCamera' && camerasList.length > 0 && 
+            <>
+            <div className='cameras__modal__title'>
+                <h2>Select a camera from your local network</h2>
+                <Close onClick={() => setIsShowModal(false)} className="cameras__modal__title_close"/>
+            </div>
+            <div className='cameras__modal__list'>
+                {
+                    camerasList.map((el, ind) => 
+                        <div key ={ind} 
+                            className='cameras__modal__list_item' 
+                            onClick={() =>  {setStage('logAndPass'); 
+                            setIPCamera(el)}}
+                        >
+                            <span>{el}</span>
+                            <AiOutlineRight/>
+                        </div>
+                    )
+                }
+            </div>   
+            </>
+        }  
+         { 
+        stage === 'logAndPass' && 
+            <>
+                <div className='cameras__modal__title'>
+                    <h2>{IPCamera}</h2>
+                    <Close onClick={() => setIsShowModal(false)} className="cameras__modal__title_close"/>
+                </div>
+                <div className='cameras__modal__login'>
+                    <label>Username</label>
+                    <input 
+                        type='text' 
+                        placeholder='Username'
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        onFocus={() => setConnectMessage('')}
+                        />
+                    <label>Password</label>
+                    <input 
+                        type='password' 
+                        placeholder='Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        onFocus={() => setConnectMessage('')}
+                    />
+                    <div style={{color:'red'}}>{connectMessage}</div>
+                    <div className='cameras__modal__login__footer'>
+                        <button className='cameras__modal__login__cancel' onClick={() => setStage('selectCamera')}>Cancel</button>
+                        <button className="cameras__modal__login__create" onClick={connect}>Connect</button>
+                    </div>
+                </div>   
+            </>
+        } 
+         { 
+        stage === 'cameraCreated' && 
+            <>
+                <div className='cameras__modal__showCamera'>
+                    <img src={`data:image/png;base64, ${localStorage.getItem(IPCamera)}`} alt='camera'/>
+                    <input type="text" value={cameraName} onChange={(e) => setCameraName(e.target.value)}/>
+                    <div className='cameras__modal__login__footer'>
+                        <button className="cameras__modal__login__create" onClick={changeCameraName}>Save</button>
+                    </div>
+                </div>   
+            </>
+        } 
+        </div>
+    </div>
+    )
+}
