471dcf0857470b4b31b492f5ba37be6b75b3f878
etosolo322 <etosolo322@yandex.ru>
2023-02-22 18:06:45 +0300

    add time

--- src/components/selectTimeDiapason.js
+++ src/components/selectTimeDiapason.js
@@ -4,35 +4,63 @@ export const SelectTimeDiapason = ({startTime, setStartTime, setEndTime, endTime
             <div>
                 <p>Starting time</p>
               <select value={startTime} onChange={(e) => setStartTime(e.target.value)} className="dashboard__time_select">
+                <option value="7:00:00">1:00</option>
                 <option value="7:00:00">7:00</option>
+                <option value="7:00:00">7:30</option>
                 <option value="8:00:00">8:00</option>
+                <option value="8:00:00">8:30</option>
                 <option value="9:00:00">9:00</option>
+                <option value="9:00:00">9:30</option>
                 <option value="10:00:00">10:00</option>
+                <option value="10:00:00">10:30</option>
                 <option value="11:00:00">11:00</option>
+                <option value="11:00:00">11:30</option>
                 <option value="12:00:00">12:00</option>
+                <option value="12:00:00">12:30</option>
                 <option value="13:00:00">13:00</option>
+                <option value="13:00:00">13:30</option>
                 <option value="14:00:00">14:00</option>
+                <option value="14:00:00">14:30</option>
                 <option value="15:00:00">15:00</option>
+                <option value="15:00:00">15:30</option>
                 <option value="16:00:00">16:00</option>
+                <option value="16:00:00">16:30</option>
                 <option value="17:00:00">17:00</option>
+                <option value="17:00:00">17:30</option>
                 <option value="18:00:00">18:00</option>
+                <option value="18:00:00">18:30</option>
                 <option value="19:00:00">19:00</option>
+                <option value="19:00:00">23:59</option>
               </select>
               <p>End time</p>
               <select value={endTime} onChange={(e) => setEndTime(e.target.value)} className="dashboard__time_select">
+              <option value="7:00:00">1:00</option>
                 <option value="7:00:00">7:00</option>
+                <option value="7:00:00">7:30</option>
                 <option value="8:00:00">8:00</option>
+                <option value="8:00:00">8:30</option>
                 <option value="9:00:00">9:00</option>
+                <option value="9:00:00">9:30</option>
                 <option value="10:00:00">10:00</option>
+                <option value="10:00:00">10:30</option>
                 <option value="11:00:00">11:00</option>
+                <option value="11:00:00">11:30</option>
                 <option value="12:00:00">12:00</option>
+                <option value="12:00:00">12:30</option>
                 <option value="13:00:00">13:00</option>
+                <option value="13:00:00">13:30</option>
                 <option value="14:00:00">14:00</option>
+                <option value="14:00:00">14:30</option>
                 <option value="15:00:00">15:00</option>
+                <option value="15:00:00">15:30</option>
                 <option value="16:00:00">16:00</option>
+                <option value="16:00:00">16:30</option>
                 <option value="17:00:00">17:00</option>
+                <option value="17:00:00">17:30</option>
                 <option value="18:00:00">18:00</option>
+                <option value="18:00:00">18:30</option>
                 <option value="19:00:00">19:00</option>
+                <option value="19:00:00">23:59</option>
               </select>
             </div>
               <div className="dashboard__time_footer">
