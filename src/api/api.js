e6efd77fdcf8fb9d2d91e0fed996762b497722fb
etosolo322 <etosolo322@yandex.ru>
2023-02-22 09:13:22 +0300

    add -

--- src/api/api.js
+++ src/api/api.js
@@ -12,7 +12,7 @@ export const API_GETALGORITHMS_I = `${url}/api/algorithms/get-process/`
 export const API_URL = `/api/safety_control/safety/action/`
 export const API_IMAGES = ``
 export const API_CAMERA = `:8008/find_cameras/`
-export const API_USERLIST = `/api/staff_control/employees/admin/`
+export const API_USERLIST = `/api/staff-control/employees/admin/`
 export const API_REGISTRATION = `/register/`
 export const API_AUTH = `/auth/jwt/create/`
 
@@ -22,7 +22,7 @@ export const API_CAMERASELECT = '/api/cameras/'
 export const API_URL_I = `${url}/api/safety_control/safety/action/`
 export const API_IMAGES_I = `${url}/`
 export const API_CAMERA_I = `${url}:8008/find_cameras/`
-export const API_USERLIST_I = `${url}/api/staff_control/employees/admin/`
+export const API_USERLIST_I = `${url}/api/staff-control/employees/admin/`
 export const API_REGISTRATION_I = `${url}/register/`
 export const API_AUTH_I = `${url}/auth/jwt/create/`
 export const API_ALGORITHM_I = `${url}/api/algorithms/available-process/`
