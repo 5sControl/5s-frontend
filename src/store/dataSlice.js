a28c929792d2b0a8321102b08a3bb4d6905b355c
etosolo322 <etosolo322@yandex.ru>
2023-02-27 09:20:16 +0300

    create currentReportReducer

---
+++ src/store/dataSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const currentReport = createSlice({
+    name:"currentReport",
+    initialState:{
+        report:false,
+    },
+    reducers:{
+        addCurrentReport(state, action){
+            state.report = action.payload;
+    },
+    }
+})
+
+export const {addCurrentReport } = currentReport.actions
+export default currentReport.reducer; 
