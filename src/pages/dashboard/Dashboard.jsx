6dc2ccaa81f9b74ff2a0d13de04e765ae7ff9f47
etosolo322 <etosolo322@yandex.ru>
2023-03-03 09:56:03 +0300

    lazy refactor filter form

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -9,11 +9,8 @@ import { Link } from "react-router-dom";
 import { Reports } from "../../components/reports/Reports";
 import { getData } from "../../api/reportsRequest";
 import { TimelineHub } from "../../components/timeline/timelineHub";
-import { SelectTimeDiapason } from "../../components/selectTimeDiapason";
-import { DataPicker } from "../../components/dataPicker";
-import { getProcess } from "../../api/algorithmRequest";
-import { parsingAlgorithmName } from "../../functions/parsingAlgorithmName";
 import {Preloader} from "../../components/preloader";
+import { FilterForm } from "./components/filter";
 
 function Dashboard() {
   const [data, setData] = useState(false);
@@ -21,34 +18,29 @@ function Dashboard() {
   const [startTime, setStartTime] = useState("7:00:00");
   const [endTime, setEndTime] = useState("19:00:00");
   const [cookies, setCookie, deleteCookie] = useCookies(["token"]);
-  const [visibleModal, setVisibleModal] = useState(false);
-  const [visibleModalDate, setVisibleModalDate] = useState(false);
   const [selectDate, setSelectDate] = useState(moment().format("YYYY-MM-DD"));
-  const [selectCameras, setSelectCameras] = useState([]);
+  
   const [cameraToResponse, setCameraToResponse] = useState("camera");
-  const [selectAlgorithm, setSelectAlgorithm] = useState([]);
   const [algorithmToResponse, setAlgorithmToResponse] = useState("algorithm");
 
 
   const update = () => {
-    setVisibleModal(false);
     getData(
       window.location.hostname,
       cookies.token,
       selectDate,
       startTime
-        .split(":")
-        .map((el, ind) => (ind === 0 ? el - 3 : el))
-        .join(":"),
+      .split(":")
+      .map((el, ind) => (ind === 0 ? el - 1 : el))
+      .join(":"),
       endTime
         .split(":")
-        .map((el, ind) => (ind === 0 ? el - 3 : el))
+        .map((el, ind) => (ind === 0 ? el - 1 : el))
         .join(":"),
       algorithmToResponse,
       cameraToResponse
     )
       .then((el) => {
-        console.log(el);
         el.data.detail === "Authentication credentials were not provided." ||
         el.data.detail === "Given token not valid for any token type"
           ? setData(0)
@@ -62,93 +54,24 @@ function Dashboard() {
   useEffect(() => {
     update();
   }, [selectDate, cameraToResponse, algorithmToResponse]);
-
-  useEffect(() => {
-    getProcess(window.location.hostname, cookies.token).then((e) => {
-      if (e.data) {
-        let bufCam = e.data;
-        bufCam = bufCam.map((el) => el.camera);
-        setSelectCameras([...new Set(bufCam)]);
-        let bufAlg = e.data;
-        bufAlg = bufAlg.map((el) => el.algorithm.name);
-        setSelectAlgorithm([...new Set(bufAlg)]);
-      }
-    });
-  }, []);
+  
   return (
     <>
       <div className="dashboard">
-        <div className="dashboard__title">
-          <h1>Dashboard</h1>
-          <div className="dashboard__title__filter">
-            <select
-              value={cameraToResponse}
-              onChange={(e) => setCameraToResponse(e.target.value)}
-              className="dashboard__title_button"
-            >
-              <option value="camera">Select cameras</option>
-              {selectCameras.map((el, ind) => {
-                return (
-                  <option value={el.id} key={ind}>
-                    {el.name}
-                  </option>
-                );
-              })}
-            </select>
-            <select
-              value={algorithmToResponse}
-              onChange={(e) => setAlgorithmToResponse(e.target.value)}
-              className="dashboard__title_button"
-            >
-              <option value="algorithm">Select algorithm</option>
-              {selectAlgorithm.map((el, ind) => {
-                return (
-                  <option value={el} key={ind}>
-                    {parsingAlgorithmName(el)}
-                  </option>
-                );
-              })}
-            </select>
-            <span className="dashboard__title_button">Sort: Newest</span>
-            <button
-              onClick={() => setVisibleModal(!visibleModal)}
-              className="dashboard__title_button"
-            >
-              {`${startTime.split(":").slice(0, 2).join(":")} - ${endTime
-                .split(":")
-                .slice(0, 2)
-                .join(":")}`}
-            </button>
-            <button
-              onClick={() => setVisibleModalDate(!visibleModalDate)}
-              className="dashboard__title_button"
-            >
-              {`${selectDate}`}
-            </button>
-          </div>
-
-          {visibleModalDate && (
-            <div className="dashboard__datapicker">
-              <DataPicker
-                setSelectDate={(e) => setSelectDate(e)}
-                update={update}
-                setVisibleModalDate={(e) => setVisibleModalDate(e)}
-                selectDateDash={selectDate}
-              />
-            </div>
-          )}
-
-          {visibleModal && (
-            <SelectTimeDiapason
-              startTime={startTime}
-              setStartTime={(e) => setStartTime(e)}
-              endTime={endTime}
-              setEndTime={(e) => setEndTime(e)}
-              update={update}
-              setVisibleModal={(e) => setVisibleModal(e)}
-            />
-          )}
-        </div>
+        <FilterForm
+          cookies={cookies}
+          setCameraToResponse = {(e) => setCameraToResponse(e)}
+          cameraToResponse = {cameraToResponse}
+          algorithmToResponse = {algorithmToResponse}
+          setAlgorithmToResponse = {(e) => setAlgorithmToResponse(e)}
+          update={update}
+          endTime={endTime}
+          setEndTime={(e) => setEndTime(e)}
+          selectDate = {selectDate}
+          setSelectDate = {(e) => setSelectDate(e)}
+          setStartTime = {(e) => setStartTime(e)}
+          startTime = {startTime}
+        />
 
         {!data ? 
         <Preloader loading={true}/> :
---
+++ src/pages/dashboard/components/filter.js
@@ -0,0 +1,103 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
+
+import { SelectTimeDiapason } from "../../../components/selectTimeDiapason";
+import { DataPicker } from "../../../components/dataPicker";
+import { parsingAlgorithmName } from "../../../functions/parsingAlgorithmName";
+
+import { getProcess } from "../../../api/algorithmRequest";
+export const FilterForm = ({cookies, setCameraToResponse, update, cameraToResponse, endTime, setEndTime, algorithmToResponse, setAlgorithmToResponse, selectDate, setSelectDate, startTime, setStartTime}) => {
+
+    const [visibleModal, setVisibleModal] = useState(false);
+    const [visibleModalDate, setVisibleModalDate] = useState(false);
+
+    const [selectAlgorithm, setSelectAlgorithm] = useState([]);
+    const [selectCameras, setSelectCameras] = useState([]);
+
+    useEffect(() => {
+        getProcess(window.location.hostname, cookies.token).then((e) => {
+          if (e.data) {
+            let bufCam = e.data;
+            bufCam = bufCam.map((el) => el.camera);
+            setSelectCameras([...new Set(bufCam)]);
+            let bufAlg = e.data;
+            bufAlg = bufAlg.map((el) => el.algorithm.name);
+            setSelectAlgorithm([...new Set(bufAlg)]);
+          }
+        });
+      }, []);
+
+    return (
+        <div className="dashboard__title">
+          <h1>Dashboard</h1>
+          <div className="dashboard__title__filter">
+            <select
+              value={cameraToResponse}
+              onChange={(e) => setCameraToResponse(e.target.value)}
+              className="dashboard__title_button"
+            >
+              <option value="camera">Select cameras</option>
+              {selectCameras.map((el, ind) => {
+                return (
+                  <option value={el.id} key={ind}>
+                    {el.name}
+                  </option>
+                );
+              })}
+            </select>
+            <select
+              value={algorithmToResponse}
+              onChange={(e) => setAlgorithmToResponse(e.target.value)}
+              className="dashboard__title_button"
+            >
+              <option value="algorithm">Select algorithm</option>
+              {selectAlgorithm.map((el, ind) => {
+                return (
+                  <option value={el} key={ind}>
+                    {parsingAlgorithmName(el)}
+                  </option>
+                );
+              })}
+            </select>
+            <span className="dashboard__title_button">Sort: Newest</span>
+            <button
+              onClick={() => setVisibleModal(!visibleModal)}
+              className="dashboard__title_button"
+            >
+              {`${startTime.split(":").slice(0, 2).join(":")} - ${endTime
+                .split(":")
+                .slice(0, 2)
+                .join(":")}`}
+            </button>
+            <button
+              onClick={() => setVisibleModalDate(!visibleModalDate)}
+              className="dashboard__title_button"
+            >
+              {`${selectDate}`}
+            </button>
+          </div>
+
+          {visibleModalDate && (
+            <div className="dashboard__datapicker">
+              <DataPicker
+                setSelectDate={(e) => setSelectDate(e)}
+                update={update}
+                setVisibleModalDate={(e) => setVisibleModalDate(e)}
+                selectDateDash={selectDate}
+              />
+            </div>
+          )}
+
+          {visibleModal && (
+            <SelectTimeDiapason
+              startTime={startTime}
+              setStartTime={(e) => setStartTime(e)}
+              endTime={endTime}
+              setEndTime={(e) => setEndTime(e)}
+              update={update}
+              setVisibleModal={(e) => setVisibleModal(e)}
+            />
+          )}
+        </div>
+    )
+}
