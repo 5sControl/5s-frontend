13514f0be7db22ca48d3c424584cf71e46e02978
etosolo322 <etosolo322@yandex.ru>
2023-03-09 19:56:58 +0300

    fix: refresh logic to checkbox

--- src/pages/main/stage/camera/modal/components/algorithmSelect.jsx
+++ src/pages/main/stage/camera/modal/components/algorithmSelect.jsx
@@ -3,8 +3,10 @@ import { getAveilableAlgorithms } from '../../../../../../api/algorithmRequest';
 import { parsingAlgorithmName } from '../../../../../../functions/parsingAlgorithmName';
 
 export const AlgorithmSelect = ({ token, algorithmsActiveObject }) => {
-  const [algorithmList, setAlgorithmList] = useState({});
-  const [selectedAlgorithm, setSelectedAlgorithm] = useState(algorithmsActiveObject);
+  const [algorithmList, setAlgorithmList] = useState(false);
+  const [selectedAlgorithm, setSelectedAlgorithm] = useState(
+    algorithmsActiveObject ? algorithmsActiveObject : []
+  );
 
   useEffect(() => {
     getAveilableAlgorithms(window.location.hostname, token).then((res) => {
@@ -14,20 +16,24 @@ export const AlgorithmSelect = ({ token, algorithmsActiveObject }) => {
   }, []);
 
   const checkboxHandler = (state) => {
+    let checkboxAlgo = [];
     if (!selectedAlgorithm.includes(state)) {
-      setSelectedAlgorithm([...selectedAlgorithm, state]);
+      checkboxAlgo.push(state);
     } else {
-      setSelectedAlgorithm(selectedAlgorithm.filter((item) => item !== state));
+      checkboxAlgo = checkboxAlgo.filter((item) => item !== state);
     }
+    console.log(checkboxAlgo);
   };
 
   return (
     <div className="cameras__settings_algorithms">
       <h1 className="cameras__settings_algorithms_title">
-        {selectedAlgorithm.length}/{algorithmList.length} <span>algorithms used</span>
+        {selectedAlgorithm ? selectedAlgorithm.length : 0}/
+        {algorithmList ? algorithmList.length : 0} <span>algorithms used</span>
       </h1>
       <div className="cameras__settings_algorithms_list">
-        {algorithmList.length > 0 &&
+        {algorithmList &&
+          algorithmList.length > 0 &&
           algorithmList.map((algorithm, index) => (
             <div key={index} className="cameras__settings_algorithms_list_item">
               <span>{parsingAlgorithmName(algorithm)}</span>
@@ -35,6 +41,7 @@ export const AlgorithmSelect = ({ token, algorithmsActiveObject }) => {
                 type="checkbox"
                 defaultChecked={selectedAlgorithm && selectedAlgorithm.includes(algorithm)}
                 onChange={() => checkboxHandler(algorithm)}
+                disabled={selectedAlgorithm && selectedAlgorithm.includes(algorithm)}
               />
             </div>
           ))}
