8f36f9b968b08fd65f3c8d286e922656683cf4a5
etosolo322 <etosolo322@yandex.ru>
2023-03-12 11:28:38 +0300

    add: added camera request

--- src/pages/newDashboard/Dashboard.jsx
+++ src/pages/newDashboard/Dashboard.jsx
@@ -1,16 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getLogs } from '../../api/algorithmRequest';
 import { useCookies } from 'react-cookie';
 
 import './live.scss';
+import { getSelectedCameras } from '../../api/cameraRequest';
 export const Dashboard = () => {
   const location = window.location.hostname;
   const [cookies] = useCookies(['token']);
+  const [cameras, setCameras] = useState([]);
   useEffect(() => {
     getLogs(location, cookies.token).then((res) => {
       console.log(res);
     });
+
+    getSelectedCameras(location, cookies.token).then((res) => {
+      setCameras(res.data ? res.data : []);
+    });
   }, []);
+
   return (
     <section className="live">
       <div className="live__title">
@@ -18,7 +25,15 @@ export const Dashboard = () => {
         <span>Calendar</span>
       </div>
       <div className="live__container">
-        <div className="live__cameras"></div>
+        <div className="live__reports">
+          <div className="live__camera">
+            {cameras.map((camera, index) => {
+              return <span key={index}>{camera.name}</span>;
+            })}
+          </div>
+          <div className="live__report-info"></div>
+        </div>
+        <div className="live__timeline"></div>
       </div>
     </section>
   );
