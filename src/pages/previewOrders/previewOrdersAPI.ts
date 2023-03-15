05e7672a3abe1f9efda5206177c5a55eadfb505b
Andrew Posohov <fessysys911@gmail.com>
2023-03-15 17:58:14 +0300

    refactor: add console.log

--- src/pages/previewOrders/previewOrdersAPI.ts
+++ src/pages/previewOrders/previewOrdersAPI.ts
@@ -4,6 +4,8 @@ import { getIsInternet, proxy, url } from '../../api/api';
 export const getOrderData = (hostname: string, cookies: string, orderId: string) => {
   const API_BY_ORDER = 'api/order/by-order';
 
+  console.log('you get getOrderData!!!', `http://${hostname}/${API_BY_ORDER}/${orderId}`);
+
   if (getIsInternet(hostname)) {
     return proxy(`${url}${API_BY_ORDER}/${orderId}`, 'GET', {
       Authorization: cookies,
