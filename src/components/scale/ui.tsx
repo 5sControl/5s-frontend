5d513485d2e024ec37a465e9701275d053b49ac8
etosolo322 <etosolo322@yandex.ru>
2023-04-18 10:58:14 +0300

    fix: change icons to fulscreen click

--- src/components/scale/ui.tsx
+++ src/components/scale/ui.tsx
@@ -1,5 +1,5 @@
 import styles from './scale.module.scss';
-import { AiOutlineFullscreen } from 'react-icons/ai';
+import { MdOutlineFullscreen } from 'react-icons/md';
 type PropsType = {
   className?: string;
   onClick: () => void;
@@ -8,7 +8,7 @@ type PropsType = {
 export const Scale: React.FC<PropsType> = ({ className, onClick }) => {
   return (
     <div className={`${styles.cover} ${className}`} onClick={onClick}>
-      <AiOutlineFullscreen style={{ color: 'white' }} />
+      <MdOutlineFullscreen style={{ color: 'white', width: '25px', height: '25px' }} />
     </div>
   );
 };
--- src/components/zoomOut/ui.tsx
+++ src/components/zoomOut/ui.tsx
@@ -1,5 +1,5 @@
 import styles from './scale.module.scss';
-import { AiOutlineFullscreenExit } from 'react-icons/ai';
+import { MdOutlineFullscreenExit } from 'react-icons/md';
 type PropsType = {
   className?: string;
   onClick: () => void;
@@ -8,7 +8,7 @@ type PropsType = {
 export const ZoomOut: React.FC<PropsType> = ({ className, onClick }) => {
   return (
     <div className={`${styles.cover} ${className}`} onClick={onClick}>
-      <AiOutlineFullscreenExit style={{ color: 'white' }} />
+      <MdOutlineFullscreenExit style={{ color: 'white', width: '25px', height: '25px' }} />
     </div>
   );
 };
