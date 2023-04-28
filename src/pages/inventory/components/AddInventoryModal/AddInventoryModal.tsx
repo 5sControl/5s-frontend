078b8fe4ca5a41aa490115812b23f980c4496c51
etosolo322 <etosolo322@yandex.ru>
2023-04-28 14:39:01 +0300

    add: add alpha new design #329

--- src/pages/inventory/components/AddInventoryModal/AddInventoryModal.tsx
+++ src/pages/inventory/components/AddInventoryModal/AddInventoryModal.tsx
@@ -27,18 +27,14 @@ export const AddInventoryModal: React.FC<PropsType> = ({ isOpen, handleClose }) 
   const { camerasData } = useAppSelector(selectInventory);
   const [cookies] = useCookies(['token']);
   const [formData, setFormData] = useState<AddInventoryData>({});
-  const [isShowCoord, setIsShowCoord] = useState<boolean>(false);
   const [coords, setCoords] = useState<Coordinat[]>([]);
   const [isClose, setIsClose] = useState<any>(false);
-  const [isCloseClick, setIsCloseClick] = useState<any>(false);
   const [itemName, setItemName] = useState<string>('');
   const [itemCount, setItemCount] = useState<number>(0);
-
+  const [currentSelect, setCurrentSelect] = useState('');
   useEffect(() => {
     if (!isOpen) {
-      setIsShowCoord(false);
       setIsClose(false);
-      setIsCloseClick(false);
       setItemName('');
       setItemCount(0);
     }
@@ -64,19 +60,23 @@ export const AddInventoryModal: React.FC<PropsType> = ({ isOpen, handleClose }) 
         : { id: '0', text: 'asd' },
     };
     setFormData(dataForm);
-    setIsShowCoord(true);
   };
 
   const submitHandler = () => {
-    const dataForm = formData;
-    dataForm.coords = coords;
+    const dataForm = {
+      name: itemName,
+      low_stock_level: itemCount,
+      camera: currentSelect,
+      coords: coords,
+    };
     setIsClose({ loading: true });
+    console.log(dataForm);
     if (coords.length > 0) {
       dispatch(
         addItem({
           token: cookies.token,
           hostname: window.location.hostname,
-          body: { ...dataForm, camera: dataForm.camera?.id },
+          body: { ...dataForm },
         })
       ).then((response: any) => {
         setIsClose({ status: response.type.includes('fulfilled'), loading: false });
@@ -92,77 +92,63 @@ export const AddInventoryModal: React.FC<PropsType> = ({ isOpen, handleClose }) 
     }
   };
 
-  const closed = () => {
-    setIsCloseClick(true);
-    // handleClose();
-  };
-
   return (
-    <Modal
-      isOpen={isOpen}
-      handleClose={handleClose}
-      className={isCloseClick ? styles.modalHide : isShowCoord ? styles.modalCoord : styles.modal}
-    >
-      {!isShowCoord && (
-        <div>
-          <div className={styles.header}>
-            <h3 className={styles.title}>Add item</h3>
-            <Close onClick={closed} />
-          </div>
-          <div className={styles.content}>
-            <form onSubmit={onSubmit}>
-              <div className={styles.input}>
-                <Input
-                  id="name"
-                  name="name"
-                  type="text"
-                  label="Item name"
-                  placeholder={'Enter item name'}
-                  value={itemName}
-                  onChange={(e: any) => setItemName(e.target.value)}
-                />
-              </div>
-              <div className={styles.input}>
-                <Input
-                  id="low_stock_level"
-                  name="low_stock_level"
-                  type="text"
-                  label="Low stock level"
-                  placeholder={'Enter number'}
-                  value={String(itemCount)}
-                  onChange={(e: any) => setItemCount(e.target.value.replace(/[^\d.]/g, ''))}
-                />
-              </div>
-              {camerasData && camerasData.length ? (
-                <div className={styles.input}>
-                  <SelectBase
-                    id="camera_type"
-                    name="camera_type"
-                    label="Select a camera"
-                    listOfData={camerasData}
-                  />
-                </div>
-              ) : null}
-              <Button
-                text="Continue"
-                className={styles.button}
-                type="submit"
-                disabled={itemName === '' || String(itemCount).length === 0}
-              />
-            </form>
-          </div>
+    <Modal isOpen={isOpen} handleClose={handleClose} className={styles.modal}>
+      <div className={styles.form}>
+        <div className={styles.header}>
+          <h3 className={styles.title}>Item settings</h3>
         </div>
-      )}
-      {isShowCoord && (
-        <Coordinates
-          submitHandler={submitHandler}
-          formData={formData}
-          setCoords={(coords: any) => setCoords(coords)}
-          setIsShowCoord={(type: boolean) => setIsShowCoord(type)}
-          closed={closed}
-        />
-      )}
-      {(isClose || isCloseClick) && (
+        <div className={styles.content}>
+          <form onSubmit={onSubmit}>
+            <div className={styles.input}>
+              <Input
+                id="name"
+                name="name"
+                type="text"
+                label="Item name"
+                placeholder={'Enter item name'}
+                value={itemName}
+                onChange={(e: any) => setItemName(e.target.value)}
+              />
+            </div>
+            <div className={styles.input}>
+              <Input
+                id="low_stock_level"
+                name="low_stock_level"
+                type="text"
+                label="Low stock level"
+                placeholder={'Enter number'}
+                value={String(itemCount)}
+                onChange={(e: any) => setItemCount(e.target.value.replace(/[^\d.]/g, ''))}
+              />
+            </div>
+            {camerasData && camerasData.length ? (
+              <div className={styles.input}>
+                <SelectBase
+                  id="camera_type"
+                  name="camera_type"
+                  label="Select a camera"
+                  listOfData={camerasData}
+                  setCurrentSelect={(select) => setCurrentSelect(select)}
+                  camerasData={camerasData}
+                />
+              </div>
+            ) : null}
+            {/* <Button
+              text="Continue"
+              className={styles.button}
+              type="submit"
+              disabled={itemName === '' || String(itemCount).length === 0}
+            /> */}
+          </form>
+        </div>
+      </div>
+      <Coordinates
+        submitHandler={submitHandler}
+        setCoords={(coords: any) => setCoords(coords)}
+        currentSelect={currentSelect}
+      />
+      {isClose && (
         <>
           {isClose && (
             <div className={styles.response}>
@@ -180,24 +166,6 @@ export const AddInventoryModal: React.FC<PropsType> = ({ isOpen, handleClose }) 
                     <p>The item is not saved</p>
                   </>
                 )}
-              </div>
-            </div>
-          )}
-          {isCloseClick && (
-            <div className={styles.ifClose}>
-              <div className={styles.ifClose__container}>
-                <div className={styles.ifClose__header}>
-                  <h2>Save changes?</h2>
-                  <span>You’ve made changes to the item. Click “Save” to keep them.</span>
-                </div>
-                <div className={styles.ifClose__footer}>
-                  <button className={styles.ifClose__footer_no} onClick={handleClose}>
-                    No
-                  </button>
-                  <button className={styles.ifClose__footer_yes} onClick={submitHandler}>
-                    Save
-                  </button>
-                </div>
               </div>
             </div>
           )}
