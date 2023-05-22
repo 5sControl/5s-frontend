323cceb7557629e6507ebb771d499d2be7e9336c
etosolo322 <etosolo322@yandex.ru>
2023-05-22 16:58:33 +0300

    add: add useless socket

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,29 +1,24 @@
-import { useEffect, useState } from 'react';
-import { getMemoryStatus } from '../api/companyRequest';
-import { useCookies } from 'react-cookie';
-
+import { useEffect } from 'react';
 import { LeftMenu } from '../components/leftMenu/leftMenu';
 import { Outlet } from 'react-router-dom';
-import { Button } from '../components/button';
-import { Warning } from '../assets/svg/SVGcomponent';
-
+import { io } from 'socket.io-client';
 import './modalDisk.scss';
 
 export const RoutesOutlet = () => {
-  const [isMemory, setIsMemory] = useState<boolean>(true);
-
-  const [cookies] = useCookies(['token']);
 
   useEffect(() => {
-    getMemoryStatus(window.location.hostname, cookies.token).then((response) =>
-      setIsMemory(response.data.has_enough_space)
+    const socket = io(
+      `http://${
+        window.location.hostname.includes('local') ? '192.168.1.110' : window.location.hostname
+      }:3456`
     );
-    const intervalId = setInterval(() => {
-      getMemoryStatus(window.location.hostname, cookies.token).then((response) =>
-        setIsMemory(response.data.has_enough_space)
-      );
-    }, 1800000);
-    return () => clearInterval(intervalId);
+    socket.on('connect', function () {
+      console.log('Connected to the server');
+    });
+
+    socket.on('notification', function (msg) {
+      console.log(msg, 'msg');
+    });
   }, []);
 
   return (
@@ -34,21 +29,6 @@ export const RoutesOutlet = () => {
           <Outlet />
         </section>
       </div>
-      {!isMemory && (
-        <section className="modalDisk">
-          <div className="modalDisk__modal">
-            <h1>Low disk space</h1>
-            <div className="modalDisk__modal_warning">
-              <Warning className="modalDisk__modal_svg" />
-            </div>
-            <p>
-              The server is running low on memory. Free up its space to avoid losing old videos that
-              will be replaced by new ones.
-            </p>
-            <Button text="Ok" onClick={() => setIsMemory(true)} />
-          </div>
-        </section>
-      )}
     </>
   );
 };
