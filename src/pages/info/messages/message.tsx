4d5ad12a06b967c25d97e1462752e735f44c2d32
etosolo322 <etosolo322@yandex.ru>
2023-05-23 15:30:25 +0300

    add: begin create layout to message 2 #388

--- src/pages/info/messages/message.tsx
+++ src/pages/info/messages/message.tsx
@@ -4,19 +4,40 @@ import { useCookies } from 'react-cookie';
 import { getSystemMessage } from '../../../api/companyRequest';
 
 import styles from './message.module.scss';
+import { Preloader } from '../../../components/preloader';
 
 export const SystemMessage = () => {
   const [messages, setMessges] = useState([]);
   const [cookies] = useCookies(['token']);
+  const [isPreloader, serIsPreloader] = useState<boolean>(true);
   useEffect(() => {
-    getSystemMessage(window.location.hostname, cookies.token).then((response) => {
-      console.log(response);
-    });
-  });
+    getSystemMessage(window.location.hostname, cookies.token)
+      .then((response) => {
+        serIsPreloader(false);
+        setMessges(response.data);
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+        serIsPreloader(false);
+      });
+  }, []);
   return (
     <section className={styles.wrapper}>
-      <div className={styles.content}></div>
-      <footer className={styles.footer}>{'< 1 >'}</footer>
+      {isPreloader ? (
+        <Preloader />
+      ) : (
+        <>
+          <div className={styles.container}>
+            {messages.length === 0 ? (
+              <div className={styles.empty}>Empty</div>
+            ) : (
+              <div>no Empty</div>
+            )}
+          </div>
+          {<footer className={styles.footer}>{'< 1 >'}</footer>}
+        </>
+      )}
     </section>
   );
 };
