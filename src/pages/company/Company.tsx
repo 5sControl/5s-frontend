228dbbc1c9dba869dfb4ef25a5f44ca15de18899
etosolo322 <etosolo322@yandex.ru>
2023-06-24 12:25:57 +0300

    add: change company page layout

--- src/pages/company/Company.tsx
+++ src/pages/company/Company.tsx
@@ -6,6 +6,9 @@ import { useCookies } from 'react-cookie';
 import { useAppDispatch } from '../../store/hooks';
 import { getCompanies, getCompanyInfoForForm, getCountries } from './companySlice';
 import { HeaderMain } from '../../components/header';
+import { CompanyInfo } from './companyTab/CompanyInfo';
+import { Contacts } from './contactsTab/Contacts';
+import { Link } from 'react-router-dom';
 
 export const Company: FC<{ activeTab: number }> = ({ activeTab }) => {
   const [cookies] = useCookies(['token']);
@@ -34,15 +37,26 @@ export const Company: FC<{ activeTab: number }> = ({ activeTab }) => {
 
   return (
     <>
-      <HeaderMain title={'Company'}></HeaderMain>
+      <HeaderMain title={'Company'}>
+        <div className="company__header">
+          <Link to="/company" className={activeTab === 0 ? 'company__header_active' : ''}>
+            Company
+          </Link>
+          <Link to="/company/contacts" className={activeTab === 1 ? 'company__header_active' : ''}>
+            Contacts
+          </Link>
+        </div>
+      </HeaderMain>
       <div className="company">
-        <Tabs activeTabDefault={activeTab} tabList={tabsData}>
+        {/* <Tabs activeTabDefault={activeTab} tabList={tabsData}>
           {tabsData.map((tab) => (
             <Tab key={tab.id} label={tab.title}>
               {tab.component}
             </Tab>
           ))}
-        </Tabs>
+        </Tabs> */}
+        {activeTab === 0 && <CompanyInfo />}
+        {activeTab === 1 && <Contacts />}
       </div>
     </>
   );
