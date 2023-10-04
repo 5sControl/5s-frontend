23cbde88b2bfd52644e8ef892600a919b1a12491
etosolo322 <etosolo322@yandex.ru>
2023-10-04 17:10:53 +0300

    fix: fixed notife api

--- src/api/notificationRequest.js
+++ src/api/notificationRequest.js
@@ -5,197 +5,68 @@ const API_SETTINGS = 'api/mailer/smtp-settings/';
 const API_EMAIL = 'api/mailer/emails/';
 const API_TASKMANAGER = ':3333/logs?taskId=';
 export const getNotificationSettings = (hostname, cookies) => {
-  if (process.env.REACT_APP_ENV === 'proxy') {
-    return proxy(process.env.REACT_APP_NGROK + API_SETTINGS, 'GET', {
+  return axios.get(`${process.env.REACT_APP_ENV}${API_SETTINGS}`, {
+    headers: {
       Authorization: cookies,
-    });
-  } else if (process.env.REACT_APP_ENV === 'wify') {
-    return axios.get(`${process.env.REACT_APP_IP_SERVER}${API_SETTINGS}`, {
-      headers: {
-        Authorization: cookies,
-      },
-    });
-  } else {
-    return axios.get(`http://${hostname}/${API_SETTINGS}`, {
-      headers: {
-        Authorization: cookies,
-      },
-    });
-  }
+    },
+  });
 };
 
 export const postNotificationSettings = async (hostname, cookies, response) => {
-  if (process.env.REACT_APP_ENV === 'proxy') {
-    return axios.post('https://5scontrol.pl/proxy_to_ngrok', {
-      url: process.env.REACT_APP_NGROK + API_SETTINGS,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: cookies,
-      },
-      body: JSON.stringify(response),
-    });
-  } else if (process.env.REACT_APP_ENV === 'wify') {
-    return axios.post(`${process.env.REACT_APP_IP_SERVER}${API_SETTINGS}`, response, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: cookies,
-      },
-    });
-  } else {
-    return axios.post(`http://${hostname}/${API_SETTINGS}`, response, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: cookies,
-      },
-    });
-  }
+  return axios.post(`${process.env.REACT_APP_NGROK}${API_SETTINGS}`, response, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: cookies,
+    },
+  });
 };
 
 export const patchNotificationSettings = (hostname, cookies, response) => {
-  if (process.env.REACT_APP_ENV === 'proxy') {
-    return proxy(
-      process.env.REACT_APP_NGROK + API_SETTINGS + '1/',
-      'PATCH',
-      {
-        Authorization: cookies,
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      JSON.stringify({
-        response,
-      })
-    );
-  } else if (process.env.REACT_APP_ENV === 'wify') {
-    return axios.patch(`${process.env.REACT_APP_IP_SERVER}${API_SETTINGS}1/`, response, {
-      headers: {
-        Authorization: cookies,
-      },
-    });
-  } else {
-    return axios.patch(`http://${hostname}/${API_SETTINGS}1/`, response, {
-      headers: {
-        Authorization: cookies,
-      },
-    });
-  }
+  return axios.patch(`${process.env.REACT_APP_NGROK}${API_SETTINGS}1/`, response, {
+    headers: {
+      Authorization: cookies,
+    },
+  });
 };
 
 export const getNotificationEmail = (hostname, cookies) => {
-  if (process.env.REACT_APP_ENV === 'proxy') {
-    return proxy(process.env.REACT_APP_NGROK + API_EMAIL, 'GET', {
+  return axios.get(`${process.env.REACT_APP_NGROK}${API_EMAIL}`, {
+    headers: {
       Authorization: cookies,
-    });
-  } else if (process.env.REACT_APP_ENV === 'wify') {
-    return axios.get(`${process.env.REACT_APP_IP_SERVER}${API_EMAIL}`, {
-      headers: {
-        Authorization: cookies,
-      },
-    });
-  } else {
-    return axios.get(`http://${hostname}/${API_EMAIL}`, {
-      headers: {
-        Authorization: cookies,
-      },
-    });
-  }
+    },
+  });
 };
 
 export const postNotificationEmail = async (hostname, cookies, response) => {
-  if (process.env.REACT_APP_ENV === 'proxy') {
-    return axios.post('https://5scontrol.pl/proxy_to_ngrok', {
-      url: process.env.REACT_APP_NGROK + API_EMAIL,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: cookies,
-      },
-      body: JSON.stringify(response),
-    });
-  } else if (process.env.REACT_APP_ENV === 'wify') {
-    return axios.post(`${process.env.REACT_APP_IP_SERVER}${API_EMAIL}`, response, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: cookies,
-      },
-    });
-  } else {
-    return axios.post(`http://${hostname}/${API_EMAIL}`, response, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: cookies,
-      },
-    });
-  }
+  return axios.post(`${process.env.REACT_APP_NGROK}${API_EMAIL}`, response, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: cookies,
+    },
+  });
 };
 
 export const patchNotificationEmail = (hostname, cookies, id, email) => {
-  if (process.env.REACT_APP_ENV === 'proxy') {
-    return proxy(
-      process.env.REACT_APP_NGROK + API_EMAIL + id + '/',
-      'PATCH',
-      {
+  return axios.patch(
+    `${process.env.REACT_APP_NGROK}${API_EMAIL}${id}/`,
+    {
+      id: id,
+      email: email,
+    },
+    {
+      headers: {
         Authorization: cookies,
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
       },
-      JSON.stringify({
-        id: id,
-        email: email,
-      })
-    );
-  } else if (process.env.REACT_APP_ENV === 'wify') {
-    return axios.patch(
-      `${process.env.REACT_APP_IP_SERVER}${API_EMAIL}${id}/`,
-      {
-        id: id,
-        email: email,
-      },
-      {
-        headers: {
-          Authorization: cookies,
-        },
-      }
-    );
-  } else {
-    return axios.patch(
-      `http://${hostname}/${API_EMAIL}${id}/`,
-      {
-        id: id,
-        email: email,
-      },
-      {
-        headers: {
-          Authorization: cookies,
-        },
-      }
-    );
-  }
+    }
+  );
 };
 
 export const deleteNotificationEmail = (hostname, cookies, id) => {
-  if (process.env.REACT_APP_ENV === 'proxy') {
-    return axios.post('https://5scontrol.pl/proxy_to_ngrok', {
-      url: `${process.env.REACT_APP_NGROK}${API_EMAIL}${id}/`,
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: cookies,
-      },
-    });
-  } else if (process.env.REACT_APP_ENV === 'wify') {
-    return axios.delete(`${process.env.REACT_APP_IP_SERVER}${API_EMAIL}${id}/`, {
-      headers: {
-        Authorization: cookies,
-      },
-    });
-  } else {
-    return axios.delete(`http://${hostname}/${API_EMAIL}${id}/`, {
-      headers: {
-        Authorization: cookies,
-      },
-    });
-  }
+  return axios.delete(`${process.env.REACT_APP_NGROK}${API_EMAIL}${id}/`, {
+    headers: {
+      Authorization: cookies,
+    },
+  });
 };
 
 export const getTaskManager = (hostname, taskId) => {
--- src/api/orderView.js
+++ src/api/orderView.js
@@ -10,30 +10,14 @@ const API_FILTRATIONDATA = 'api/new-order/filtration-data';
 const API_STATUSDATA = 'api/connector/status/';
 
 export const getOrderViewOperations = (hostname, cookies, startDate, endDate) => {
-  if (process.env.REACT_APP_ENV === 'proxy') {
-    return proxy(
-      `${process.env.REACT_APP_NGROK}${API_OPERATIONS}?from=${startDate}&to=${endDate}`,
-      'GET',
-      {
-        Authorization: cookies,
-      }
-    );
-  } else if (process.env.REACT_APP_ENV === 'wify') {
-    return axios.get(
-      `${process.env.REACT_APP_IP_SERVER}${API_OPERATIONS}?from=${startDate}&to=${endDate}`,
-      {
-        headers: {
-          Authorization: cookies,
-        },
-      }
-    );
-  } else {
-    return axios.get(`http://${hostname}/${API_OPERATIONS}?from=${startDate}&to=${endDate}`, {
+  return axios.get(
+    `${process.env.REACT_APP_NGROK}${API_OPERATIONS}?from=${startDate}&to=${endDate}`,
+    {
       headers: {
         Authorization: cookies,
       },
-    });
-  }
+    }
+  );
 };
 
 export const getOrderViewOrderList = (hostname, cookies, startDate, endDate) => {
