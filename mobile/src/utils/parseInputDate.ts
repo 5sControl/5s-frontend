accf30cde967e4046703e71c2ee1588bc7ae5c3e
Polina Kostukevich <kostukevich.p9@gmail.com>
2024-12-05 13:47:15 +0300

    fix: use inline iso date function

--- mobile/src/utils/parseInputDate.ts
+++ mobile/src/utils/parseInputDate.ts
@@ -151,14 +151,7 @@ export const updateTimeInDate = (dateString: string): string => {
 
 export const getCurrentDateTimeISO = (): string => {
   const now = new Date();
-  const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, '0');
-  const day = String(now.getDate()).padStart(2, '0');
-  const hours = String(now.getHours()).padStart(2, '0');
-  const minutes = String(now.getMinutes()).padStart(2, '0');
-  const seconds = String(now.getSeconds()).padStart(2, '0');
-
-  return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
+  return now.toISOString();
 }
 
 export const getDateWeekAgoISO = (): string => {
