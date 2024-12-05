f0bcb9b5dd8a27ce09025f96d540d1c2bbdc26c9
ThanhDoIVan <thanhkahp2002@gmail.com>
2024-12-05 12:09:59 +0300

    feat: qr localization

--- mobile/src/components/qrScanner/qrCustomScanner.tsx
+++ mobile/src/components/qrScanner/qrCustomScanner.tsx
@@ -1,46 +1,30 @@
-import { useIonViewDidEnter, useIonViewWillLeave } from "@ionic/react";
+import { useIonViewWillLeave } from "@ionic/react";
 import { Html5Qrcode } from "html5-qrcode";
 import { useEffect, useRef, useState } from "react";
 import "./qrScanner.scss";
-import { useHistory } from "react-router";
-import { ROUTES } from "../../shared/constants/routes";
+import { useTranslation } from "react-i18next";
 
 type QrCodeProps = {
   qrCodeSuccessCallback: (decodedText: string, decodedResult: any) => void;
 };
 
 const QrCode = ({ qrCodeSuccessCallback }: QrCodeProps) => {
+  const { t } = useTranslation();
   const qrCodeReaderRef = useRef<Html5Qrcode | null>(null);
   const [scanning, setScanning] = useState(false);
   const [qrCode, setQrCode] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
-  // let hasScanned = false;
-  // let initialized = false;
 
   useEffect(() => {
-    // hasScanned = false;
     const scanner = new Html5Qrcode("qr-reader");
     qrCodeReaderRef.current = scanner;
   }, []);
 
   useIonViewWillLeave(() => {
     const scanner = qrCodeReaderRef?.current;
-    // if (scanner && hasScanned) {
-    //   scanner.pause();
-    //   initialized = true;
-    // }
     if (scanner) {
       stopScanning();
     }
-  });
-
-  useIonViewDidEnter(() => {
-    // hasScanned = false;
-    const scanner = qrCodeReaderRef?.current;
-    // if (scanner && initialized && !hasScanned) {
-    //   scanner.resume();
-    // }
-    // stopScanning();
   });
 
   const startScanning = () => {
@@ -56,7 +40,6 @@ const QrCode = ({ qrCodeSuccessCallback }: QrCodeProps) => {
       (decodedText, decodedResult) => {
         if (qrCode !== decodedText) {
           if (decodedResult) {
-            // hasScanned = true;
             qrCodeSuccessCallback(decodedText, decodedResult);
           } else {
             console.warn("Unsupported format detected:", decodedResult);
@@ -77,7 +60,6 @@ const QrCode = ({ qrCodeSuccessCallback }: QrCodeProps) => {
         .then(() => {
           setScanning(false);
           setQrCode(null);
-          // hasScanned = false;
         })
         .catch(err => {
           console.error("Failed to stop scanning:", err);
@@ -89,7 +71,7 @@ const QrCode = ({ qrCodeSuccessCallback }: QrCodeProps) => {
     <div className="qr__container">
       <div id="qr-reader"></div>
       <button className="qr__button" onClick={scanning ? stopScanning : startScanning}>
-        {scanning ? "Stop Scanning" : "Start Scanning"}
+        {scanning ? t("scanner.stop") : t("scanner.start")}
       </button>
     </div>
   );
