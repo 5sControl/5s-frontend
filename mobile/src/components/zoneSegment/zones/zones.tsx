3d99fd639a1d894c4c302938eb3e07101ed6d4df
ThanhDoIVan <thanhkahp2002@gmail.com>
2024-12-26 14:20:00 +0300

    fix: zone overlap and creating an arbitrary shape

--- mobile/src/components/zoneSegment/zones/zones.tsx
+++ mobile/src/components/zoneSegment/zones/zones.tsx
@@ -1,26 +1,21 @@
-import { useEffect, useState } from 'react';
-import styles from './zones.module.scss';
-import { ZonesCoordinates } from './coordinates/zonesCoordinates';
-import { ZoneList } from './zoneList/zoneList';
-import {
-  getCameraZones,
-  patchCameraZones,
-  postCameraZones,
-  deleteCameraZones,
-} from '../../../api/cameraRequest';
-import { useCookies } from 'react-cookie';
-import { NoVideoBig } from '../../../assets/svg/SVGcomponent';
-import { getWorkplaceList } from '../../../api/ordersView';
-import { Preloader } from '../../preloader/preloader';
-import { Notification } from '../../notification/notification';
-import { IonToggle } from '@ionic/react';
-import { useTranslation } from 'react-i18next';
+import { useEffect, useState } from "react";
+import styles from "./zones.module.scss";
+import { ZonesCoordinates } from "./coordinates/zonesCoordinates";
+import { ZoneList } from "./zoneList/zoneList";
+import { getCameraZones, patchCameraZones, postCameraZones, deleteCameraZones } from "../../../api/cameraRequest";
+import { useCookies } from "react-cookie";
+import { NoVideoBig } from "../../../assets/svg/SVGcomponent";
+import { getWorkplaceList } from "../../../api/ordersView";
+import { Preloader } from "../../preloader/preloader";
+import { Notification } from "../../notification/notification";
+import { IonToggle } from "@ionic/react";
+import { useTranslation } from "react-i18next";
 
 const Zones = ({ cameraSelect, isCreateCamera }) => {
   const [coords, setCoords] = useState<any>([]);
-  const [itemName, setItemName] = useState('');
+  const [itemName, setItemName] = useState("");
   const [isScale, setIsScale] = useState(false);
-  const [cookie] = useCookies(['token']);
+  const [cookie] = useCookies(["token"]);
   const [cameraZones, setCameraZones] = useState([]);
   const [currentZoneId, setCurrentZoneId] = useState<number>();
   const [workplaceList, setWorkplaceList] = useState([]);
@@ -34,29 +29,130 @@ const Zones = ({ cameraSelect, isCreateCamera }) => {
   const [zoneType, setZoneType] = useState(2);
   const { t } = useTranslation();
 
+  const getRectanglePoints = coords => {
+    if (Object.keys(coords).length < 8) {
+      const { x1, y1, x2, y2 } = coords;
+
+      return {
+        x1: Math.min(x1, x2),
+        y1: Math.min(y1, y2),
+        x2: Math.max(x1, x2),
+        y2: Math.min(y1, y2),
+        x3: Math.max(x1, x2),
+        y3: Math.max(y1, y2),
+        x4: Math.min(x1, x2),
+        y4: Math.max(y1, y2),
+      };
+    }
+
+    return coords;
+  };
+
+  const orientation = (p, q, r) => {
+    const val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+    return val === 0 ? 0 : val > 0 ? 1 : 2;
+  };
+
+  const onSegment = (p, q, r) => {
+    if (
+      q.x <= Math.max(p.x, r.x) &&
+      q.x >= Math.min(p.x, r.x) &&
+      q.y <= Math.max(p.y, r.y) &&
+      q.y >= Math.min(p.y, r.y)
+    )
+      return true;
+    return false;
+  };
+
+  const doLinesIntersect = (p1, q1, p2, q2) => {
+    const o1 = orientation(p1, q1, p2);
+    const o2 = orientation(p1, q1, q2);
+    const o3 = orientation(p2, q2, p1);
+    const o4 = orientation(p2, q2, q1);
+
+    return (
+      (o1 !== o2 && o3 !== o4) ||
+      (o1 === 0 && onSegment(p1, p2, q1)) ||
+      (o2 === 0 && onSegment(p1, q2, q1)) ||
+      (o3 === 0 && onSegment(p2, p1, q2)) ||
+      (o4 === 0 && onSegment(p2, q1, q2))
+    );
+  };
+
+  const isPointInPolygon = (point, polygon) => {
+    let x = point.x,
+      y = point.y;
+    let inside = false;
+    for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+      let xi = polygon[i].x,
+        yi = polygon[i].y;
+      let xj = polygon[j].x,
+        yj = polygon[j].y;
+      let intersect = yi > y != yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
+      if (intersect) inside = !inside;
+    }
+    return inside;
+  };
+
+  const pointsToArray = coords => {
+    const newCoords = getRectanglePoints(coords);
+    return [
+      { x: newCoords.x1, y: newCoords.y1 },
+      { x: newCoords.x2, y: newCoords.y2 },
+      { x: newCoords.x3, y: newCoords.y3 },
+      { x: newCoords.x4, y: newCoords.y4 },
+    ];
+  };
+
+  const isOverlapping = (coords1, coords2) => {
+    const polygon1 = pointsToArray(coords1);
+    const polygon2 = pointsToArray(coords2);
+
+    for (let i = 0; i < polygon1.length; i++) {
+      if (isPointInPolygon(polygon1[i], polygon2)) {
+        return true;
+      }
+    }
+    for (let i = 0; i < polygon2.length; i++) {
+      if (isPointInPolygon(polygon2[i], polygon1)) {
+        return true;
+      }
+    }
+
+    for (let i = 0, j = polygon1.length - 1; i < polygon1.length; j = i++) {
+      for (let k = 0, l = polygon2.length - 1; k < polygon2.length; l = k++) {
+        if (doLinesIntersect(polygon1[i], polygon1[j], polygon2[k], polygon2[l])) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  };
+
   const getZone = () => {
     setPreloader(true);
     getCameraZones(window.location.hostname, cookie.token, cameraSelect.id)
-      .then((res) => {
+      .then(res => {
         setCameraZones(res.data);
         setCurrentZoneId(-2);
         setCoords([]);
         setPreloader(false);
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
         setPreloader(false);
       });
   };
 
-  const deleteZone = (id) => {
+  const deleteZone = id => {
     setPreloader(true);
     deleteCameraZones(window.location.hostname, cookie.token, id)
       .then(() => {
         setMessage({ status: true, message: t("camera.zoneSegment.messages.deleted") });
         getZone();
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
         setMessage({ status: false, message: t("camera.zoneSegment.messages.notDeleted") });
         setPreloader(false);
@@ -65,6 +161,8 @@ const Zones = ({ cameraSelect, isCreateCamera }) => {
 
   const saveZone = () => {
     const otherZones: any = cameraZones.filter((box: any) => box.id !== currentZoneId);
+    console.log("other Zones", otherZones);
+    console.log("save coords", coords);
 
     if (coords.length === 0) {
       setMessage({ status: false, message: t("camera.zoneSegment.messages.select") });
@@ -73,20 +171,22 @@ const Zones = ({ cameraSelect, isCreateCamera }) => {
 
     if (!validZone) {
       setMessage({ status: false, message: t("camera.zoneSegment.messages.notSaved") });
-      setHandleSaveError((prev) => !prev);
+      setHandleSaveError(prev => !prev);
       return;
     }
 
-    for (let i = 0; i < otherZones.length; i++) {
-      for (let n = 0; n < otherZones[i].coords.length; n++) {
-        const otherCoords = otherZones[i].coords[n];
+    const overlap = coords.some((figure, index) =>
+      coords.slice(index + 1).some(otherFigure => isOverlapping(figure, otherFigure))
+    );
 
-        const overlap = !(
-          coords[0].x1 > otherCoords.x2 ||
-          coords[0].x2 < otherCoords.x1 ||
-          coords[0].y1 > otherCoords.y2 ||
-          coords[0].y2 < otherCoords.y1
-        );
+    if (overlap) {
+      setMessage({ status: false, message: t("camera.zoneSegment.messages.overlap") });
+      return;
+    }
+
+    for (const otherZone of otherZones) {
+      for (const otherCoords of otherZone.coords) {
+        const overlap = coords.some(coord => isOverlapping(coord, otherCoords));
 
         if (overlap) {
           setMessage({ status: false, message: t("camera.zoneSegment.messages.overlap") });
@@ -98,8 +198,8 @@ const Zones = ({ cameraSelect, isCreateCamera }) => {
     const body: any = {
       coords: coords.map(coord => ({
         ...coord,
-        zoneType: zoneType
-    })),
+        zoneType: zoneType,
+      })),
       camera: cameraSelect.id,
       name: itemName,
     };
@@ -114,7 +214,7 @@ const Zones = ({ cameraSelect, isCreateCamera }) => {
           getZone();
           setMessage({ status: true, message: t("camera.zoneSegment.messages.saved") });
         })
-        .catch((error) => {
+        .catch(error => {
           console.log(error);
           setPreloader(false);
           setMessage({ status: false, message: t("camera.zoneSegment.messages.notSaved") });
@@ -126,7 +226,7 @@ const Zones = ({ cameraSelect, isCreateCamera }) => {
           getZone();
           setMessage({ status: true, message: t("camera.zoneSegment.messages.saved") });
         })
-        .catch((error) => {
+        .catch(error => {
           console.log(error);
           setPreloader(false);
           setMessage({ status: false, message: t("camera.zoneSegment.messages.notSaved") });
@@ -139,21 +239,21 @@ const Zones = ({ cameraSelect, isCreateCamera }) => {
     getZone();
 
     getWorkplaceList(cookie.token)
-      .then((res) => {
+      .then(res => {
         setWorkplaceList(
-          res.data.map((place) => {
+          res.data.map(place => {
             return {
-            ...place,
-            value: place.operationName,
-            label: place.operationName
+              ...place,
+              value: place.operationName,
+              label: place.operationName,
             };
           })
         );
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error);
-      })
-    }, []);
+      });
+  }, []);
 
   useEffect(() => {
     if (message) {
@@ -175,19 +275,19 @@ const Zones = ({ cameraSelect, isCreateCamera }) => {
 
   return (
     <>
-        <div className={`${styles.zones} ion-padding`}>
+      <div className={`${styles.zones} ion-padding`}>
         {isCreateCamera ? (
-        <section className={styles.creating}>
-          <img src={NoVideoBig} />
-          <p>{t("camera.zoneSegment.notConnected")}</p>
-        </section>
-      ) : (
+          <section className={styles.creating}>
+            <img src={NoVideoBig} />
+            <p>{t("camera.zoneSegment.notConnected")}</p>
+          </section>
+        ) : (
           <ZonesCoordinates
             currentSelect={cameraSelect.id}
-            setCoords={(coords) => setCoords(coords)}
+            setCoords={coords => setCoords(coords)}
             itemName={itemName}
             isScale={isScale}
-            setIsScale={(e) => setIsScale(e)}
+            setIsScale={e => setIsScale(e)}
             cameraBox={cameraZones.filter((box: any) => box.id !== currentZoneId)}
             oldBox={cameraZones.filter((box: any) => box.id === currentZoneId)}
             currentZoneId={currentZoneId}
@@ -196,27 +296,27 @@ const Zones = ({ cameraSelect, isCreateCamera }) => {
             setValidZone={setValidZone}
             isFourPointsMode={zoneType === 4}
           />
-      )}
-          <div className={styles.zones__right}>
-            <ZoneList
-              saveZone={saveZone}
-              deleteZone={deleteZone}
-              cameraZones={cameraZones}
-              setItemName={(name) => setItemName(name)}
-              itemName={itemName}
-              setCurrentZoneId={(id) => setCurrentZoneId(id)}
-              currentZoneId={isCreateCamera ? -3 : currentZoneId}
-              setWorkplaceToSend={(e) => setWorkplaceToSend(e)}
-              workplaceList={workplaceList}
-              workplace={workplaceToSend ? workplaceToSend.comboBoxName : ''}
-              isNewZone={createZoneMode}
-              setIsNewZone={setCreateZoneMode}
-            />
-            {/* <div className={styles.zonesSwitcher}>
-            <IonToggle checked={zoneType === 4} onChange={(e) => setZoneType(e ? 4 : 2)}>4 points zone mode</IonToggle>
-          </div> */}
+        )}
+        <div className={styles.zones__right}>
+          <ZoneList
+            saveZone={saveZone}
+            deleteZone={deleteZone}
+            cameraZones={cameraZones}
+            setItemName={name => setItemName(name)}
+            itemName={itemName}
+            setCurrentZoneId={id => setCurrentZoneId(id)}
+            currentZoneId={isCreateCamera ? -3 : currentZoneId}
+            setWorkplaceToSend={e => setWorkplaceToSend(e)}
+            workplaceList={workplaceList}
+            workplace={workplaceToSend ? workplaceToSend.comboBoxName : ""}
+            isNewZone={createZoneMode}
+            setIsNewZone={setCreateZoneMode}
+          />
+          <div className={styles.zonesSwitcher}>
+            <IonToggle onIonChange={e => setZoneType(e.detail.checked ? 4 : 2)}>4 points zone mode</IonToggle>
           </div>
         </div>
+      </div>
       {preloader && (
         <div className={styles.preloader}>
           <Preloader />
