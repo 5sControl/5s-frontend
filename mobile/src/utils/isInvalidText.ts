d160a07c29177ea9ad7738c6ca655ed46dde62c4
Kiril Baskakov <kirill_baskakov12@mail.ru>
2025-01-10 05:51:44 -0800

    fix: fix isinvalid function

--- mobile/src/utils/isInvalidText.ts
+++ mobile/src/utils/isInvalidText.ts
@@ -1,17 +1,21 @@
-export const isInvalidText = (value: string | undefined, allowNumbers?: boolean, allowSpaces: boolean = true) => {
+interface Allows {
+    numbers?: boolean;
+    spaces?: boolean;
+}
+
+export const isInvalidText = (value: string | undefined, allows: Allows = {}) => {
+    const { numbers = false, spaces = false } = allows;
+
     if (!value) return false;
-
-    let regex: RegExp;
-
-    if (allowNumbers) {
-        regex = allowSpaces 
-            ? /[^a-zA-Zа-яА-ЯёЁ0-9 _-]/ 
-            : /[^a-zA-Zа-яА-ЯёЁ0-9_-]/;
-    } else {
-        regex = allowSpaces 
-            ? /[^a-zA-Zа-яА-ЯёЁ _-]/ 
-            : /\s|[^a-zA-Zа-яА-ЯёЁ_-]/;
+    
+    let allowedSymbols = '-_a-zA-Zа-яА-ЯёЁ';
+    if (numbers) {
+        allowedSymbols += '0-9';
+    } 
+    if (spaces) {
+        allowedSymbols += ' ';
     }
 
-    return regex.test(value);
+    const regex = new RegExp(`[^${allowedSymbols}]`); 
+    return regex.test(value); 
 }
